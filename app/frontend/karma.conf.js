9de3434a5cf98497733b3c449ea2c66573833b54
Ch-Valentine <senioro.valentino@gmail.com>
2016-12-15 00:36:14 +0200

    added the UI testing workflow, npm scripts for running tests in terminal by PhantomJs and on real browser

--- app/frontend/karma.conf.js
+++ app/frontend/karma.conf.js
@@ -1,46 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = function(config) {
   config.set({
-    // browsers: ['Chrome', 'Firefox', 'Safari'], before git pull
-    browsers: ['Chrome'],
-    // karma only needs to know about the test bundle
+    autoWatch: true,
+    frameworks: ['chai', 'mocha', 'sinon'],
     files: [
+      'node_modules/babel-polyfill/dist/polyfill.js',
+      'node_modules/phantomjs-polyfill/bind-polyfill.js',
       'tests.webpack.js'
     ],
-    frameworks: ['chai', 'mocha'],
-    plugins: [
-      'karma-safari-launcher',
-      'karma-firefox-launcher',
-      'karma-chrome-launcher',
-      'karma-mocha-reporter',
-      'karma-chai',
-      'karma-mocha',
-      'karma-sourcemap-loader',
-      'karma-webpack'
-    ],
-    // run the bundle through the webpack and sourcemap plugins
     preprocessors: {
       'tests.webpack.js': ['webpack', 'sourcemap']
     },
-    // reporters: ['mocha'],
-    singleRun: false,
-    // webpack config object
     webpack: {
-      devtool: 'inline-source-map',
+      resolve: {
+        extensions: ['.js', '.jsx', '.scss', '.css']
+      },
       module: {
-        loaders: [
-          {
+        rules: [
+          { test: /\.(jsx|js)?$/,
             exclude: /node_modules/,
-            loader: "babel-loader",
-            test: /\.js?$/
-          }
+            loader: 'babel-loader',
+            query: { babelrc: false, presets: ["es2015", "react", "stage-1"], plugins: ["transform-decorators-legacy"] }
+          },
+          { test: /\.json$/, loader: 'json-loader' },
+          { test: /\.css$/,  use: ['style-loader', 'css-loader', 'postcss-loader'] },
+          { test: /\.scss$/, use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'] },
+          { test: /\.less$/, use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader'] },
+          { test: /\.gif$/,  loader: 'url-loader?mimetype=image/png' },
+          { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+          { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=[name].[ext]' }
         ]
-      }
+      },
+      externals: {
+        'jsdom': 'window',
+        'cheerio': 'window',
+        'react/lib/ExecutionEnvironment': true,
+        'react/addons': true,
+        'react/lib/ReactContext': true,
+        'sinon': 'window.sinon'
+      },
+      plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.ProvidePlugin({
+          'global.sinon': 'sinon',
+          'window.sinon': 'sinon',
+          'sinon': 'sinon'
+        })
+      ]
     },
     webpackMiddleware: {
+      stats: 'errors-only',
       noInfo: true
     },
-    client: {
-      captureConsole: false
+    watchOptions: {
+      aggregateTimeout: 100
     }
   });
 };
